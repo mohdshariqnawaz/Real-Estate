a238b7a55eaf3917ff892b0f002ade886baf1507
Mohammed Shariq Nawaz <mohdshariqnawaz@gmail.com>
2018-04-12 21:29:46 +0530

    Added Search functinality and cleaned some code

--- FSProject2018/index.py
+++ FSProject2018/index.py
@@ -14,27 +14,23 @@ class estateDetails:
 
 	def getdata( self):
 		print("Enter the estate name")
-		estateName= raw_input()
+		estateName= input()
 		print("Enter the estate address")                                               
-		estateAddress = raw_input()
+		estateAddress = input()
 		print("Enter the estate size")
-		estateSize = raw_input()
+		estateSize = input()
 		print("Enter the estate price")
-		estatePrice =raw_input()
+		estatePrice =input()
 		print("Enter the estate owner")
-		estateOwner = raw_input()
+		estateOwner = input()
 		print("Enter the estate condition")
-		estateCondition = raw_input()
+		estateCondition = input()
 
 	def putdata(self ):
 		#st="the details are %s mm %s m %s m %s n %s m %s" %(self.estateName+self.estateAddress+self.estateSize+self.estatePrice+self.estateOwner+self.estateCondition)
 		st=("\nESTATE NAME -"+self.estateName+"\nESTATE ADDRESS - "+self.estateAddress+"\nESTATE SIZE -"+self.estateSize+"\nESTATE PRICE - "+self.estatePrice+"\nESTATE OWNER -"+
 			self.estateOwner+"\nESTATE CONDITION -"+self.estateCondition)
 		print(st)
-	
-
-
-
 
 if __name__== "__main__":
 	estateObj=estateDetails()
@@ -43,27 +39,52 @@ if __name__== "__main__":
 	print("\nENTER YOUR CHOICE")
 	while(1):
 		print("\n 1-ADD AN ESTATE DETAIL \n 2-SEARCH FOR AN ESTATE\n 3-VISUALISE THE ESTATES\n 4-EXIT THE PORTAL")
-		choice= raw_input()
+		choice= input()
 		if(choice=="1"):
-			print("enter the following details estateName\nestateAddress\nestateSize\nestatePrice\nestateOwner\nestateCondition\n")
+			print("enter the following details\nestateName\nestateAddress\nestateSize\nestatePrice\nestateOwner\nestateCondition\n")
 			text_file= open("estatedetails.txt","a")
 			for i in range (1, 7):
 			    print("Please enter data: ")
-			    line = raw_input("\n") 
+			    line = input("\n") 
 			    word_list.append(line + "|")
-			
-			text_file.writelines(word_list+"/") 
-			text_file.close() 
+			word_list.append("\n")
+			text_file.writelines(word_list) 
+			text_file.close()
 
+		elif(choice=="2"):
+			print("Searching...")
+			print("Enter the Name of the estate to be searched")
+			searchParam = input("\n")
+			search_file = open("estatedetails.txt","r")
+			for estates in search_file:
+				if searchParam in estates:
+					print("Estate Found!!!")
+					print(estates)
+			search_file.close()
 
-
+			print("Do you wish to modify??\n Press Y or N Please...")
+			ch=input()
+			if ch=='Y':
+				print("enter the modified estate details\nestateName\nestateAddress\nestateSize\nestatePrice\nestateOwner\nestateCondition\n")
+				for i in range (1, 7):
+				    print("Please enter data: ")
+				    line = input("\n") 
+				    word_list.append(line + "|")
 				
+				modify_file = open("estatedetails.txt","r")
+				searchParam.replace(searchParam,modify_file.writelines(word_list))
+				modify_file.close()
+
+		elif(choice=="3"):
+			print("Visualising...")
+
+		elif(choice=="4"):
+			exit()
 
 
-	
 
-	#estateObj.getdata()
-	#estateObj.putdata()
+  #estateObj.getdata()
+  #estateObj.putdata()
 
 
 
